caaacf146ab6d9b2c599cee84c9d6512249795b1
Navjot Singh <Navjot Singh>
2017-02-22 14:06:13 +0100

    database is working

--- pages/studentfeedback.php
+++ pages/studentfeedback.php
@@ -9,31 +9,31 @@
         <div id="divDifficulty" >
             <h1>How difficult do you feel the lecture is right now?</h1>
             <center>
-                <form action="pages/submitDifficult.php" method="POST" target="target">
-                    <input type="image" src="img/veryEasyRect.png" name="difficultyValue" value="0">
-                    <input type="image" src="img/easyRect.png" name="difficultyValue" value="1">
-                    <input type="image" src="img/okRect.png" name="difficultyValue" value="2">
-                    <input type="image" src="img/hardRect.png" name="difficultyValue" value="3">
-                    <input type="image" src="img/veryHardRect.png" name="difficultyValue" value="4">
+                <form action="index.php?page=submitSpeed" method="POST">
+                    <button type="submit" name="difficultyValue" value="0"><img src="img/verySlowRect.png" alt="Submit"></button>
+                    <button type="submit" name="difficultyValue" value="1"><img src="img/slowRect.png" alt="Submit"></button>
+                    <button type="submit" name="difficultyValue" value="2"><img src="img/okRect.png" alt="Submit"></button>
+                    <button type="submit" name="difficultyValue" value="3"><img src="img/fastRect.png" alt="Submit"></button>
+                    <button type="submit" name="difficultyValue" value="4"><img src="img/veryFastRect.png" alt="Submit"></button>
                 </form>
             </center>
         </div>
         <div id="divSpeed">
             <h1>How fast do you feel the lecture is progressing right now?</h1>
             <center>
-                <form action="pages/submitSpeed.php" method="POST" target="target">
-                    <input type="image" src="img/verySlowRect.png" name="speedValue" value="0">
-                    <input type="image" src="img/slowRect.png" name="speedValue" value="1">
-                    <input type="image" src="img/okRect.png" name="speedValue" value="2">
-                    <input type="image" src="img/fastRect.png" name="speedValue" value="3">
-                    <input type="image" src="img/veryFastRect.png" name="speedValue" value="4">
+                <form action="index.php?page=submitSpeed" method="POST">
+                    <button type="submit" name="speedValue" value="0"><img src="img/verySlowRect.png" alt="Submit"></button>
+                    <button type="submit" name="speedValue" value="1"><img src="img/slowRect.png" alt="Submit"></button>
+                    <button type="submit" name="speedValue" value="2"><img src="img/okRect.png" alt="Submit"></button>
+                    <button type="submit" name="speedValue" value="3"><img src="img/fastRect.png" alt="Submit"></button>
+                    <button type="submit" name="speedValue" value="4"><img src="img/veryFastRect.png" alt="Submit"></button>
                 </form>
             </center>
         </div>
         <div id="divComment">
             <h1>Do you have any comments or questions?</h1>
             <div id="divTextFieldAndButton">
-                <form action="pages/submitText.php" method="POST" target="target">
+                <form action="index.php?page=submitSpeed" method="POST" target="target">
                     <textarea name="textFeedback" rows="3" cols="30">The cat was playing in the garden.</textarea><br><br>
                     <input type="submit" value="Submit">
                 </form>
--- pages/submitSpeed.php
+++ pages/submitSpeed.php
@@ -1,32 +1,37 @@
 <!-- Made by Henrik on 15.02.17 --> 
 
 <?php
-    isset($_POST['speedValue']);
-    echo("Hi" . (int)($_POST['speedValue']) . "is the speed you think the lecture is progressing at.");
-    $valueToSend = (int)($_POST['speedValue']);
-     
-    //Getting the table from the DB
-    global $conn;
-    $sql = "INSERT INTO Feedback (speed, difficulty, time, lectureId)
-    VALUES ($valueToSend, 2,2017-03-11,2)";
-    
-    echo($sql);
+    if (isset($_POST['speedValue'])) {
+        echo("Hi " . (int)($_POST['speedValue']) . " is the speed you think the lecture is progressing at.");
+        $valueToSend = (int)($_POST['speedValue']);
+        
+        //Getting the table from the DB
+        global $conn;
+        $sql = "INSERT INTO Feedback (speed, difficulty, time, lectureId)
+        VALUES ($valueToSend, NULL,NOW(),1)";
+        
+        echo("<br><br>" . $sql);
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "mydb";
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "mydb";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        } else {
+            echo("<br>Connected");
+        }
+        if (mysqli_query($conn, $sql)) {
+        echo "<br><br>New record created successfully";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
     } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo("Didn't get any speed value!");
     }
 
 ?>
