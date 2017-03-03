498fb676258d01bb2eae6050b669683664fb4695
Henrik Høybakk Olsvik <henro@stud.ntnu.no>
2017-03-03 21:29:33 +0100

    Did userstory done & completed an extra userstory
    site now blocks malicious use. Site also has adaptive meters based on
    Ajax weighted feedback

--- pages/lecturerFeedback.php
+++ pages/lecturerFeedback.php
@@ -24,56 +24,25 @@
                 $lectureName = $rowLect["lectureName"];
             }
         }
-
-        //variabler for å ha oversikt over verdiene på lecture
-        $speedScore = 2;
-        $speedCount = 1;
-        $difficultyScore = 2;
-        $difficultyCount = 1;
-        //sjekker at innholdet er der
-        if (!$resultFeed) {
-            echo(mysqli_error($conn));
-        } else {
-            if (mysqli_fetch_assoc($resultFeed) > 0) {
-                //itererer gjennom Feedback-entiteten etter lecturer med id 2 og samler speed- og difficultyverdier
-                while ($row = mysqli_fetch_assoc($resultFeed)) {
-                if ($row["lectureId"] == $lectureId) {
-                        if (!($row["speed"] == NULL)) {
-                            $speedScore = $speedScore + $row["speed"];
-                            $speedCount = $speedCount + 1;
-                        }
-                    }
-                }
-            }
-        }
-        $resultFeed = mysqli_query($conn, $sqlFeed);
-        if (!$resultFeed) {
-            echo(mysqli_error($conn));
-        } else {
-            if (mysqli_fetch_assoc($resultFeed) > 0) {
-                while ($row = mysqli_fetch_assoc($resultFeed)) {
-                    if ($row["lectureId"] == $lectureId) {
-                        if (!($row["difficulty"] == NULL)) {
-                            $difficultyScore = $difficultyScore + $row["difficulty"];
-                            $difficultyCount = $difficultyCount + 1;
-                        }
-                    }
-                }
-            }
-            //regner ut gjennomsnittsscorene
-            $speedScore = $speedScore / $speedCount;
-            $difficultyScore = $difficultyScore / $difficultyCount;
-        }
     ?>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
+
+        var fart;
+        var vanskelig;
+
+        
         google.charts.load('current', {'packages':['gauge']});
         google.charts.setOnLoadCallback(drawChart);
+
+        //Function for å oppdatere metere
         function drawChart() {
+
+            //Grafikk og verdi beskrivelser
             var data = google.visualization.arrayToDataTable([
                 ['Label', 'Value'],
-                ['Fart', 10],
-                ['Vanskelighetsgrad', 10]
+                ['Fart', 50],
+                ['Vanskelighetsgrad', 50]
             ]);
 
             var options = {
@@ -83,27 +52,73 @@
                 greenFrom:33, greenTo: 66,
                 minorTicks: 2
             };
-
+            
             var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 
-            var fart = ( <?php echo( $speedScore ) ?> /4)*100; //regner ut verdien til farten
-            var vanskelig = (<?php echo( $difficultyScore ) ?> /4)*100; //regner ut verdien til vanskelighetsgraden
+            updatesMeters();
+
+            //Oppsett av verdier
+            //fart = ( <?php echo( $speedScore ) ?> /4)*100; //regner ut verdien til farten
+            //vanskelig = (<?php echo( $difficultyScore ) ?> /4)*100; //regner ut verdien til vanskelighetsgraden
+            
 
             chart.draw(data, options);
 
+            //Regelmessig oppdatering av metere
             setInterval(function() {
                 data.setValue(0, 1, fart);
                 chart.draw(data, options);
-            }, 100);
+            }, 1000);
             setInterval(function() {
                 data.setValue(1, 1, vanskelig);
                 chart.draw(data, options);
-            }, 100);
+            }, 1000);
         }
+
+        //Oppdaterer verdiene som meterene settes til
+        function updateMeterValues(meterValues){
+
+            var fartNum = (parseFloat(meterValues[1]) + 2);
+            var hardNum = (parseFloat(meterValues[3]) + 2);
+            var fartCount = (parseFloat(meterValues[2]) + 1 );
+            var hardCount = (parseFloat(meterValues[4]) + 1 );
+            
+            fart = ((fartNum / fartCount)/4)*100; 
+            vanskelig = ((hardNum / hardCount)/4)*100;
+
+        }
+
+        //Function to update the difficulty and speed meters
+        function updatesMeters(){
+            //Obtaining Lecture ID to pass on
+            var lectureID = "<?php echo $lectureId ?>";
+            //Ajax magic (set connection to script and run)
+            var xhttp;
+            xhttp = new XMLHttpRequest();
+            var data = "lectureID=" + lectureID;
+            xhttp.open("POST", "index.php?page=updateLecturerRatings", true);
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhttp.send(data);
+            xhttp.onreadystatechange = function(){
+                if (xhttp.readyState == 4) {  
+                    if (xhttp.status == 200) { 
+                        //String pharsing using € as divider to exclude unneeded headers
+                        var meterValues = xhttp.responseText.split("€");
+                        //alert(xhttp.responseText);
+                        updateMeterValues(meterValues);
+                    } else {  
+                    alert('There was a problem with the request.');  
+                    }  
+                }
+            }
+        }
+        
+        window.setInterval(function(){updatesMeters();}, 5000);
+
     </script>
     <title>Actifeed</title>
     </head>  
-    <body>
+    <body> 
         <div id="info">
             <h1>Emne: <?php echo ($lectureName) ?> </h1>
             <h2>Foreleser: <?php echo ($foreleser) ?> </h2>
