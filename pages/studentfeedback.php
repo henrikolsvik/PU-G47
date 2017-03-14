62b64b9634ca64e78010d93c8d3c1ad6cc0eb523
RovelMan <jotdeepgahla@hotmail.com>
2017-03-14 15:35:06 +0100

    moved some js code

--- pages/studentfeedback.php
+++ pages/studentfeedback.php
@@ -16,87 +16,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <script>
-            
-            //Controls how many seconds may pass between each type of feedback
-            var secsBetween = 1*60000;
-            var lastComment = "0"
-            var lastDifficulty = "0"
-            var lastSpeed = "0"
-
-
-            //validation of time between feedbacks
-            function feedbackControl(feedbackType){
-                var currentTime = new Date();
-                if(feedbackType.id == "comment"){
-                    if(lastComment+secsBetween > currentTime.getTime()){
-                        alert("You need to wait a bit with your comment");
-                        feedbackType.action = "";
-                        return 1;
-                    }
-                    else{
-                        feedbackType.action = "index.php?page=submitText";
-                        lastComment = currentTime.getTime();
-                    }
-                    
-                }
-                else if(feedbackType.id == "speed"){
-                    if(lastSpeed+secsBetween > currentTime.getTime()){
-                        alert("You need to wait a bit with your feedback");
-                        feedbackType.action = "";
-                        return 1;
-                    }
-                    else{
-                        feedbackType.action = "index.php?page=submitSpeed";
-                        lastSpeed = currentTime.getTime();
-                    }
-                }
-                else if(feedbackType.id == "difficulty"){
-                    if(lastDifficulty+secsBetween > currentTime.getTime()){
-                        alert("You need to wait a bit with your feedback");
-                        feedbackType.action = "";
-                        return 1;
-                    }
-                    else{
-                        feedbackType.action = "index.php?page=submitDifficult";
-                        lastDifficulty = currentTime.getTime();
-                    }
-
-                }
-                return 0;
-            }  
-
-
-            function changeColor(sender){
-                var i = feedbackControl(sender);
-
-                //skips effect to signal lack of success
-                if(i == 0){
-                    color="green";
-                    document.body.style.background = color;
-                    setTimeout(function(){ 
-                        color = "#67BAB2";
-                        document.body.style.background = color; 
-                        }, 500);
-                }
-                return true;
-            }
-
-            function textEffect(sender){
-                var i = feedbackControl(sender);
-                
-                //skips effect to signal lack of success
-                if(i == 0){
-                    document.getElementById('statusSend').innerHTML = 'Feedback Sent!';
-                    document.getElementById('statusSend').style.color = 'green';
-                    setTimeout(function(){ 
-                        document.getElementById('statusSend').innerHTML = 'Please submit your feedback for lecture: <?php echo($lectureName) ?>';
-                        document.getElementById('statusSend').style.color = 'black';
-                    }, 500);
-                }
-                return true; 
-            }
-        </script>
+        <script type="text/javascript" src="js/studentFeedbackController.js"></script>
     </head>
     <body> 
         <div class="logo">  
@@ -132,7 +52,7 @@
         <div class="divQuestion"> 
             <h2>DO YOU HAVE ANY COMMENTS?</h2> 
             <div id="divComment"> 
-                <form id="comment" action="index.php?page=submitText" method="POST" target="target" onSubmit="return textEffect(this)">
+                <form id="comment" action="index.php?page=submitText" method="POST" target="target" onSubmit="return changeColor(this)">
                     <input type=hidden name="lecID" value=<?php echo($lectureID) ?>> 
                     <center>
                     <input class="commentField" type="text" name="textFeedback" placeholder="Comment..."><br>
