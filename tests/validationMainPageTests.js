72a44677c42f384c73bcd186efe7db5b0c974217
Henrik Høybakk Olsvik <henro@stud.ntnu.no>
2017-04-03 10:11:29 +0200

    nearly full tests for mainpage js

--- tests/validationMainPageTests.js
+++ tests/validationMainPageTests.js
@@ -6,11 +6,18 @@ QUnit.test("testing null detection in idInputField", function( assert ){
 
 //In the far off future this test maybe become a false negative.
 QUnit.test("testing non-existing id in idInputField", function( assert ){
-    document.getElementById("selectLectureID").innerHTML = "999";
-    idArray = ['0','999','221', '1'];
+    document.getElementById("selectLectureID").value = "999";
+    idArray = ['0','900','221', '1'];
     assert.ok(checkNullStudent() == false, "non-existing id was detected");
 });
 
+QUnit.test("testing correct id in idInputField", function( assert ){
+    document.getElementById("selectLectureID").value = "999";
+    idArray = ['0','999','221', '1'];
+    assert.ok(checkNullStudent() == true, "was approved");
+});
+
+//Lecturer tests
 QUnit.test("check of no password lecturer  detection", function( assert ){
      document.getElementById("passwordL").value = "";
      document.getElementById("usernameL").value = "Donald Trump";
@@ -20,9 +27,17 @@ QUnit.test("check of no password lecturer  detection", function( assert ){
 QUnit.test("check of no username lecturer detection", function( assert ){
      document.getElementById("usernameL").value = "";
      document.getElementById("passwordL").value = "rewq";
-     assert.ok(checkNullLecturer() == false, "no password; was denied");
+     assert.ok(checkNullLecturer() == false, "no username; was denied");
 });
 
+QUnit.test("check of correct lecturer detection", function( assert ){
+     document.getElementById("usernameL").value = "Donald Trump";
+     document.getElementById("passwordL").value = "rewq";
+     nameArray = ['Donald Trump','Jarl Kristiansen'];
+     assert.ok(checkNullLecturer() == true, "was approved");
+});
+
+//Admin tests
 QUnit.test("check of administrator no username detection", function( assert ){
      document.getElementById("usernameA").value = "";
      document.getElementById("passwordA").value = "rewq";
@@ -33,4 +48,11 @@ QUnit.test("check of administrator no password detection", function( assert ){
      document.getElementById("passwordA").value = "";
      document.getElementById("usernameA").value = "Donald Trump";
      assert.ok(checkNullAdmin() == false, "no password was denied");
+});
+
+QUnit.test("check of administrator no password detection", function( assert ){
+     document.getElementById("passwordA").value = "rewq";
+     document.getElementById("usernameA").value = "Donald Trump";
+     adminArray = ['Donald Trump'];
+     assert.ok(checkNullAdmin() == true, "was approved");
 });
